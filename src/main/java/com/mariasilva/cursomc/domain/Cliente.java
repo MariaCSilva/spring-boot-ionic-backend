084ba1b4af2aafd2860ca8c24af7a3943c95dafe
Maria Silva <maria.correia.s@gmail.com>
2019-04-21 14:32:52 -0300

    Endpoint /clientes/{id} disponível

--- src/main/java/com/mariasilva/cursomc/domain/Cliente.java
+++ src/main/java/com/mariasilva/cursomc/domain/Cliente.java
@@ -15,6 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.mariasilva.cursomc.domain.enums.TipoCliente;
 @Entity
 public class Cliente  implements Serializable{
@@ -28,6 +29,7 @@ public class Cliente  implements Serializable{
 	private String cpfOuCnpj;
 	private Integer  tipo;
 	
+	@JsonManagedReference
 	@OneToMany(mappedBy = "cliente" )
 	private List<Endereco> enderecos = new ArrayList<>();
 	
---
+++ src/main/java/com/mariasilva/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.mariasilva.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mariasilva.cursomc.domain.Cliente;
+import com.mariasilva.cursomc.repositories.ClienteRepository;
+import com.mariasilva.cursomc.services.Exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Cliente obj = repo.findOne(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: "+id +", Tipo "+Cliente.class.getName());
+		}
+		return obj;
+	}
+}
