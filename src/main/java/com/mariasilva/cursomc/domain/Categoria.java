cab5372baf75730cdbf3b0b2b0c16f556eb64495
Maria Silva <maria.correia.s@gmail.com>
2019-04-21 10:14:56 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/mariasilva/cursomc/domain/Categoria.java
+++ src/main/java/com/mariasilva/cursomc/domain/Categoria.java
@@ -9,6 +9,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
  
 @Entity
 public class Categoria implements Serializable{
@@ -19,6 +21,8 @@ public class Categoria implements Serializable{
 	private Integer id;
 	private String nome;
 	
+	//A anotação JsonManagedReference -> fica do lado que quer que venha os objetos associados
+	@JsonManagedReference
 	@ManyToMany(mappedBy="categorias")
 	private List<Produto> produtos = new ArrayList<>();
 	
--- src/main/java/com/mariasilva/cursomc/services/CategoriaService.java
+++ src/main/java/com/mariasilva/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.mariasilva.cursomc.domain.Categoria;
 import com.mariasilva.cursomc.repositories.CategoriaRepository;
+import com.mariasilva.cursomc.services.Exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,6 +15,9 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: "+id +", Tipo "+Categoria.class.getName());
+		}
 		return obj;
 	}
 }
